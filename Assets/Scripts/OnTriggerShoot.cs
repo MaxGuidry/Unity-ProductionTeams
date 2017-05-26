204134bdd6fa6facf0f8d62d5255946d28265a09
dylanmitchell1007 <armadummy@yahoo.com>
2017-05-26 18:03:48 -0500

    Fix my mess up

---
+++ Assets/Scripts/OnTriggerShoot.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnTriggerShoot : MonoBehaviour
+{
+    public GameObject Plane;
+    public GameObject ammo;
+    public GameObject spawner;
+  
+    public int speed;
+
+    public void Shoot()
+    {
+        this.spawner = (GameObject) Instantiate(this.spawner, this.spawner.transform.position, this.spawner.transform.rotation);
+        var spawner = (GameObject)Instantiate(ammo, this.ammo.transform.position, this.ammo.transform.rotation);
+        spawner.GetComponent<Rigidbody>().velocity = ammo.transform.forward * speed;
+        Destroy(spawner, 2.0f);
+        Shoot();
+    }
+
+    private void OnTriggerEnter(Collider other, GameObject target)
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Shoot();
+        }
+        if (other.tag == "Player")
+            Shoot();
+        print("Encounterd");
+
+    }
+
+    private void OnTriggerStay(Collider other)
+        {
+            if (other.tag == "Player")
+                Shoot();
+            Debug.Log("Encounterd");
+
+
+        }
+    }
+
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    void Update()
+    {
+
+
+        if (Input.GetKey(KeyCode.A))
+            this.transform.position += new Vector3(-0.1f, 0, 0);
+        if (Input.GetKey(KeyCode.W))
+            this.transform.position += new Vector3(0, 0, .1f);
+        if (Input.GetKey(KeyCode.S))
+            transform.position += new Vector3(0, 0, -.1f);
+        if (Input.GetKey(KeyCode.D))
+            this.transform.position += new Vector3(.1f, 0, 0);
+    }
+}
