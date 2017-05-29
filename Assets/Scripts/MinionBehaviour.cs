617c8ed5149fdaaf6191afc38a7ddce8400c6d0e
MaxGuidry <mguidry7@outlook.com>
2017-05-29 10:44:56 -0500

    spawns better Minions SO added Tower SO added

--- Assets/Scripts/MinionBehaviour.cs
+++ Assets/Scripts/MinionBehaviour.cs
@@ -2,30 +2,62 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MinionBehaviour : MonoBehaviour {
-
+public class MinionBehaviour : MonoBehaviour
+{
+    [HideInInspector]
+    public Minion minion;
+    private GameObject PlayerTower;
+    private GameObject EnemyTower;
+    private Vector3 targetTower;
+    Tower twr;
     private UnityEngine.AI.NavMeshAgent nav;
-	// Use this for initialization
+    // Use this for initialization
     void OnEnable()
     {
-        this.gameObject.AddComponent<DestroyForTesting>().time = 10f;
+        //this.gameObject.AddComponent<DestroyForTesting>().time = 10f;
     }
-	void Start () {
-        
+    void Awake()
+    {
+        minion = ScriptableObject.CreateInstance<Minion>();
+        twr = ScriptableObject.CreateInstance<Tower>();
+        twr.health = 1000;
+
+    }
+    void Start()
+    {
         nav = GetComponent<UnityEngine.AI.NavMeshAgent>();
         nav.Warp(this.transform.position);
-        if (this.gameObject.transform.position.z < 0)
+        PlayerTower = GameObject.FindGameObjectWithTag("Player Tower");
+        EnemyTower = GameObject.FindGameObjectWithTag("Enemy Tower");
+
+        if (Vector3.Distance(this.transform.position, PlayerTower.transform.position) < Vector3.Distance(this.transform.position, EnemyTower.transform.position))
+        {
+            targetTower = EnemyTower.transform.position;
+            minion.minionType = Minion.MinionType.PLAYER;
+        }
+        else
+        {
+            targetTower = PlayerTower.transform.position;
+            minion.minionType = Minion.MinionType.ENEMY;
+        }
+            if (this.gameObject.transform.position.z < 0)
         {
             nav.SetDestination(GameObject.FindGameObjectWithTag("LeftBridge").transform.position);
         }
         else
             nav.SetDestination(GameObject.FindGameObjectWithTag("RightBridge").transform.position);
-        
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        if (Vector3.Distance(this.transform.position, nav.destination) < 1)
-            nav.SetDestination(new Vector3(-20, 1, 0));
-	}
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector3.Distance(this.transform.position, nav.destination) < 3)
+            nav.SetDestination(targetTower);
+
+        if(Vector3.Distance(this.transform.position,targetTower)<3)
+        {
+            StartCoroutine(minion.Attack(twr));
+        }
+    }
 }
---
+++ Assets/Scripts/POCOS/Minion.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Minion : ScriptableObject
+{
+
+    public int health;
+    public int damage;
+    public enum MinionType
+    {
+        PLAYER,
+        ENEMY,
+    }
+    public MinionType minionType; 
+    public IEnumerator Attack(Tower target)
+    {
+        while (true)
+        {
+            target.health -= damage;
+            yield return new WaitForSeconds(3);
+        }
+    }
+}
