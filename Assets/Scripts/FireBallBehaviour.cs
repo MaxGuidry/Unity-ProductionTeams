ab885bcc38cc1e672183945d36c3c769cdf2c86f
dylanmitchell1007 <armadummy@yahoo.com>
2017-06-07 17:46:17 -0500

    6/7/2017

---
+++ Assets/Scripts/FireBallBehaviour.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireBallBehaviour : MonoBehaviour
+{
+
+    public GameObject Ammo;
+    public GameObject Spawner;
+
+
+    public int Speed;
+
+    public void Shoot()
+    {
+
+        
+        var spawn = (GameObject)Instantiate(Ammo, Spawner.transform.position, Spawner.transform.rotation);
+        spawn.GetComponent<Rigidbody>().velocity = this.transform.forward * Speed;
+       
+
+
+        Destroy(spawn, 2.0f);
+    }
+
+    private void DamageMinion(Collider other)
+    {
+
+        //if (Ammo.transform.position == Target.transform.position)
+            ScriptableObject.CreateInstance<Minion>().health = 0;
+        GetComponent<MinionBehaviour>().minion.health = 0;
+       
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Minion")
+            Shoot();
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Minion")
+            Shoot();
+    }
+
+    void Update()
+    {
+
+        //if (Ammo.transform.position == Target.transform.position)
+        {
+            
+            ScriptableObject.CreateInstance<Minion>().health = 0;
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Shoot();
+            Destroy(Ammo, 1.20f);
+        }
+
+    }
+}
---
+++ Assets/Scripts/FireBallDamage.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireBallDamage : MonoBehaviour
+{
+    public GameObject FireBall;
+    public MinionBehaviour min;
+    public ScriptableObject Damage;
+    private void DamageMinion(Collider other)
+    {
+        if (FireBall.transform.position == min.transform.position)
+            min.minion.damage = 0;
+            min.minion.health = 0;
+            GetComponent<MinionBehaviour>().minion.health = 0;
+        if (other.tag == "Minion")
+            GetComponent<MinionBehaviour>().minion.health = 0;
+        else
+        {
+            if (other.tag == "Fireball")
+            {
+                GetComponent<MinionBehaviour>().minion.health = 0;
+                
+            }
+
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
