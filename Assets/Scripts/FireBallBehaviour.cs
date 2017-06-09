0158fa73e7d47f831634ea8bbb129d3ef0e4762c
dylanmitchell1007 <armadummy@yahoo.com>
2017-06-09 16:10:55 -0500

    06/09/2017

--- Assets/Scripts/FireBallBehaviour.cs
+++ Assets/Scripts/FireBallBehaviour.cs
@@ -11,13 +11,17 @@ public class FireBallBehaviour : MonoBehaviour
     public GameObject RightHand;
     public int Speed;
 
-    public void Shoot()
+    public void ShootLeft()
     {
         var left = (GameObject) Instantiate(Ammo, LeftHand.transform.position, LeftHand.transform.rotation);
-        var right = (GameObject)Instantiate(Ammo, RightHand.transform.position, RightHand.transform.rotation);
         left.GetComponent<Rigidbody>().velocity = LeftHand.transform.right * -1 * Speed;
-        right.GetComponent<Rigidbody>().velocity = RightHand.transform.right * Speed;
         Destroy(left, 2.0f);
+    }
+
+    public void ShootRight()
+    {
+        var right = (GameObject)Instantiate(Ammo, RightHand.transform.position, RightHand.transform.rotation);
+        right.GetComponent<Rigidbody>().velocity = RightHand.transform.right * Speed;
         Destroy(right, 2.0f);
     }
 
@@ -37,7 +41,8 @@ public class FireBallBehaviour : MonoBehaviour
      
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            Shoot();
+            ShootRight();
+            ShootLeft();
             Destroy(Ammo, 1.20f);
         }
 
