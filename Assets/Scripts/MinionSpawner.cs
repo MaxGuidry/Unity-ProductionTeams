8a823daedf31985b9e48262fe25ec35ff73ca0ef
MaxGuidry <mguidry7@outlook.com>
2017-06-09 12:47:34 -0500

    STUFF

--- Assets/Scripts/MinionSpawner.cs
+++ Assets/Scripts/MinionSpawner.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class MinionSpawner : MonoBehaviour
@@ -11,62 +12,83 @@ public class MinionSpawner : MonoBehaviour
     public bool active;
     [HideInInspector]
     public float SpawnCooldown = 10f;
+
+    private bool auto;
     void Start()
     {
-        //Time.timeScale =2;
+        if (Vector3.Distance(GameObject.FindGameObjectWithTag("Player Tower").transform.position,
+                this.transform.position) < 50)
+            auto = false;
+        else
+        {
+            auto = true;
+        }
+            //Time.timeScale =2;
         active = true;
-        StartCoroutine(spawner());
+        if(auto)
+            StartCoroutine(spawner());
     }
     IEnumerator spawner()
     {
         while (active)
         {
-            GameObject go = Instantiate<GameObject>(minion);
-            go.transform.position = this.transform.position;
-            Minion min = go.GetComponent<MinionBehaviour>().minion;
+           
             
 
             if (Time.time < 200)
             {
-                min.health = 100;
-                min.damage = 5;
+            
+                Spawn(100,5);
                 SpawnCooldown = 20;
             }
             else if (Time.time < 400)
             {
-                min.health = 125;
-                min.damage = 7;
+                
+                Spawn(125, 7);
                 SpawnCooldown = 15;
             }
             else if(Time.time > 400 && Time.time < 430)
             {
                 //Harder minions
-                min.health = 500;
-                min.damage = 50;
-                go.transform.localScale = new Vector3(2, 2, 2);
+                
+                Spawn(500, 50);
+
                 SpawnCooldown = 45;
             }
             else if(Time.time < 700)
             {
-                min.health = 200;
-                min.damage = 20;
+               
+                Spawn(200, 20);
+
                 SpawnCooldown = 10;
             }
             //add boss minions 
             else if(Time.time <1200)
             {
-                min.health = 250;
-                min.damage = 30;
+                
+                Spawn(250, 30);
+
                 SpawnCooldown = 7;
             }
             else
             {
-                min.health = 300;
-                min.damage = 40;
+                
+                Spawn(300, 40);
+
                 SpawnCooldown = 5;
             }
             yield return new WaitForSeconds(SpawnCooldown);
         }
+    }
+
+   
+    public void Spawn(int h ,int d)
+    {
+        GameObject go = Instantiate<GameObject>(minion);
+        go.transform.position = this.transform.position;
+        Minion min = go.GetComponent<MinionBehaviour>().minion;
+        min.health = h;
+        min.damage = d;
     }
 
 }
---
+++ Assets/Scripts/POCOS/Player.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : ScriptableObject, IDamager
+{
+
+    public int health;
+    public int damage;
+    public float xp;
+    public float maxXP;
+    public int crystals;
+    public int level;
+    [SerializeField]
+    private GameObject spawner1;
+    [SerializeField]
+    private GameObject spawner2;
+    public void DoDamage(IDamagable target)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void SpawnMinion()
+    {
+        if (crystals > 25)
+        {
+            int i = Random.Range(1, 3);
+            switch (i)
+            {
+                case 1:
+                {
+                    spawner1.GetComponent<MinionSpawner>().Spawn(100 * level, 5 * level);
+                    break;
+                }
+                case 2:
+                {
+                    spawner2.GetComponent<MinionSpawner>().Spawn(100 * level, 5 * level);
+                    break;
+                }
+            }
+            crystals -= 25;
+        }
+    }
+
+}
