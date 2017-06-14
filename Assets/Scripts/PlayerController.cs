b8eea681e3a043b6df5becf0e1530c6e54f65e84
MaxGuidry <mguidry7@outlook.com>
2017-06-13 22:00:34 -0500

    work on player

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using System.Linq;
 public class PlayerController : MonoBehaviour
 {
     public Animator anim;
@@ -10,9 +11,13 @@ public class PlayerController : MonoBehaviour
     public Wizard wiz;
     private bool attacking = false;
     private bool targeting = false;
+    public int crystals;
     private GameObject target;
+    public GameObject myTower;
+
     void Start()
     {
+        crystals =100;
         agent = GetComponent<NavMeshAgent>();
         wiz = ScriptableObject.CreateInstance<Wizard>();
         wiz.damage = 10;
@@ -40,6 +45,7 @@ public class PlayerController : MonoBehaviour
         {
             if (target.GetComponent<MinionBehaviour>().minion.health <= 0)
             {
+                crystals += target.GetComponent<MinionBehaviour>().minion.damage * 5;   
                 anim.SetTrigger("targetdead");
                 targeting = false;
                 attacking = false;
@@ -67,5 +73,52 @@ public class PlayerController : MonoBehaviour
         target = go;
         agent.SetDestination(go.transform.position);
         targeting = true;
+    }
+    public void SpawnMinion()
+    {
+        if (crystals >= 50)
+        {
+            var spawners = GetComponentsInChildren<MinionSpawner>().ToList();
+
+            if (Time.time < 200)
+            {
+                spawners[0].Spawn(Random.Range(90, 116), Random.Range(4, 7));
+                spawners[1].Spawn(Random.Range(90, 116), Random.Range(4, 7));
+            }
+            else if (Time.time < 400)
+            {
+                spawners[0].Spawn(Random.Range(118, 131), Random.Range(5, 11));
+                spawners[1].Spawn(Random.Range(118, 131), Random.Range(5, 11));
+            }
+            else if (Time.time > 400 && Time.time < 430)
+            {
+                //Harder minions
+
+                spawners[0].Spawn(Random.Range(500, 751), Random.Range(50, 76));
+                spawners[1].Spawn(Random.Range(500, 751), Random.Range(50, 76));
+            }
+            else if (Time.time < 700)
+            {
+                spawners[0].Spawn(Random.Range(190, 216), Random.Range(17, 26));
+                spawners[1].Spawn(Random.Range(190, 216), Random.Range(17, 26));
+            }
+            //add boss minions 
+            else if (Time.time < 1200)
+            {
+
+
+                spawners[0].Spawn(Random.Range(240, 276), Random.Range(24, 41));
+                spawners[1].Spawn(Random.Range(240, 276), Random.Range(24, 41));
+            }
+            else
+            {
+                spawners[0].Spawn(Random.Range(280, 351), Random.Range(35, 51));
+                spawners[1].Spawn(Random.Range(280, 351), Random.Range(35, 51));
+            }
+        }
+        else
+        {
+            //do a thing
+        }
     }
 }
