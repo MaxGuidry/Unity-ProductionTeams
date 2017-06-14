15f41fe57c7ff4caa17635003587952f7d1e25db
dylanmitchell1007 <armadummy@yahoo.com>
2017-06-14 17:38:02 -0500

    06/14/2017

---
+++ Assets/Scripts/MouseCursor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseCursor : MonoBehaviour
+{
+
+    public Texture2D curserTexture;
+    public CursorMode cursorMode = CursorMode.Auto;
+    public Vector2 hotspot = Vector2.zero;
+
+    void Start()
+    {
+        Cursor.SetCursor(curserTexture, hotspot, cursorMode);
+    }    
+}
